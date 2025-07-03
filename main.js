f317176bfb94f9ace56540b17971b0f37f601b66
Matheus <matheus271112@gmail.com>
2025-07-03 16:39:32 -0300

    Finalizando versao 1.1

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 const whatsapp = document.getElementById('whatsapp');
 const modalWhats =  document.getElementById('modal-whatsapp')
 whatsapp.addEventListener('click', function() {
-    modalWhats.classList.add('modal-whatsapp')
+    modalWhats.classList.remove('hidden')
 })
 
 modalWhats.addEventListener('click', function() {
-    this.classList.remove('modal-whatsapp')
+    this.classList.add('hidden')
 })
 
 })
